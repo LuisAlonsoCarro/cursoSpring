bbfeea380c3b245673b66d733e8558cfd2ea6808
Equipo_02 <Equipo_02@DESKTOP-ERL0JIM>
2019-03-27 18:15:50 +0100

    Aplicacion Web de Spring en otro paquete

---
+++ ws/spring empleados/src/profe/empleados/web/controller/HomeController.java
@@ -0,0 +1,15 @@
+package profe.empleados.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+	@RequestMapping("saludos")
+	public String home()
+	{
+		return "index";
+	}
+}
