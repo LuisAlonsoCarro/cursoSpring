b89e15996f2d766547d4900487b88473fdcc41a0
Equipo_02 <Equipo_02@DESKTOP-ERL0JIM>
2019-03-28 19:42:15 +0100

    Ultimo commit

---
+++ ws/spring empleados/src/profe/empleados/exception/EmpDuplicadoException.java
@@ -0,0 +1,30 @@
+package profe.empleados.exception;
+
+public class EmpDuplicadoException extends EmpException {
+
+	public EmpDuplicadoException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public EmpDuplicadoException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public EmpDuplicadoException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public EmpDuplicadoException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public EmpDuplicadoException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- ws/spring empleados/src/profe/empleados/web/controller/HomeController.java
+++ ws/spring empleados/src/profe/empleados/web/controller/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import profe.empleados.negocio.EmpNegocio;
@@ -16,10 +17,17 @@ public class HomeController {
 	private EmpNegocio empNegocio;
 	
 //	@RequestMapping("saludos")
+	@RequestMapping(method = RequestMethod.GET)
 	public String muestraFormulario()
 	{
-		
 //		model.addAttribute("empleado", empNegocio.getEmpleado(cif));
+		return "empleados";
+	}
+	
+	@RequestMapping(params = {"listar"}, method = RequestMethod.POST)
+	public String muestraRespuestaLista(Model model) {
+		model.addAttribute("listaEmpleados", empNegocio.getAllEmpleados());
+		model.addAttribute("opcion", "muestraTodos");
 		return "empleados";
 	}
 }
